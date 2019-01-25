21c7d6bddfc2e2fd04024c4729531f24f835d920
Lawrence Craft <Lawrence.Craft@viavisolutions.com>
2019-01-25 21:41:44 +0000

    Sample deflate64

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val commonsCompress = "org.apache.commons" % "commons-compress" % "1.18"
 }
---
+++ src/main/scala/deflate64test/Deflate64DecompressionTest.scala
@@ -0,0 +1,87 @@
+package deflate64test
+
+import java.io.InputStream
+
+import cats.effect.{ExitCode, IO, IOApp, Resource}
+import cats.implicits._
+import org.apache.commons.compress.archivers.zip.{ZipArchiveEntry, ZipFile}
+
+import scala.annotation.tailrec
+
+object Deflate64DecompressionTest extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = args match {
+    case archiveName :: fileName :: Nil =>
+      import Kompressor._
+      import ConsoleIO._
+
+      for {
+        file <- openZipFile(archiveName)
+        innerEntries <- file.extractEntries
+        entryMap = innerEntries.map(zae => zae.getName -> zae).toMap
+        _ <- putStrLn("Files in archive:")
+        _ <- entryMap.keys.toList.traverse(putStrLn)
+        exitCode <- entryMap.get(fileName) match {
+          case Some(entry) =>
+            val stream = openStream(file, entry)
+            for {
+              count <- stream.use(enumerateStream)
+              _ <- putStrLn(s"The byte size of the entry is $count")
+            } yield ExitCode.Success
+          case None =>
+            ConsoleIO.putError(s"Cannot find the file $fileName in the archive") *> IO(ExitCode.Error)
+        }
+      } yield exitCode
+
+    case _ =>
+      ConsoleIO.putError("Please supply an argument of the file you want to introspect") *> IO(ExitCode.Error)
+  }
+}
+
+object ConsoleIO {
+  def putStrLn(message: String): IO[Unit] = IO(println(message))
+
+  def putError(message: String): IO[Unit] = IO(System.err.println(message))
+}
+
+object Kompressor {
+  def openZipFile(f: String): IO[ZipFile] = IO(new ZipFile(f))
+
+  def openStream(file: ZipFile, entry: ZipArchiveEntry): Resource[IO, InputStream] = {
+    val acquireStream = IO(file.getInputStream(entry))
+    Resource.make(acquireStream)(stream => IO(stream.close()))
+  }
+
+  def enumerateStream(stream: InputStream): IO[Long] = {
+    val buf = new Array[Byte](8096)
+
+    @tailrec
+    def loop(soFar: Long): Long = {
+      val read = stream.read(buf)
+      if (read > 0) {
+        loop(soFar + read)
+      } else {
+        soFar
+      }
+    }
+
+    IO(loop(0))
+  }
+
+  implicit class ZipFileSafeOps(zipFile: ZipFile) {
+    val extractEntries: IO[List[ZipArchiveEntry]] = IO(zipFile.getEntries).map(enumeratorToList)
+
+    private def enumeratorToList[E](enm: java.util.Enumeration[E]): List[E] = {
+      @tailrec
+      def loop(soFar: List[E]): List[E] = {
+        if (enm.hasMoreElements) {
+          loop(enm.nextElement :: soFar)
+        } else {
+          soFar
+        }
+      }
+
+      loop(Nil).reverse
+    }
+  }
+
+}
